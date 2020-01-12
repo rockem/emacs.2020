6d05430d6a2fa9c2d366b1cf47df74299ef64e2c
Eli Segal <eli.segal@cs-robotics.com>
2020-01-12 18:35:40 +0200

    More improvements

--- init.el
+++ init.el
@@ -21,10 +21,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (git-gutter magit counsel evil use-package))))
+ '(package-selected-packages
+   (quote
+    (log4j-mode auto-complete git-gutter magit counsel evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
