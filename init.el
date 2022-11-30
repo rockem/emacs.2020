36407d697bb856894b7519ca6b761edeb5024c57
Eli Segal <eli.segal@gmail.com>
2022-11-30 08:53:57 +0200

    Lots of small tweaks

--- init.el
+++ init.el
@@ -34,8 +34,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (groovy-mode nord-theme highlight-numbers rainbow-mode diminish exec-path-from-shell csv-mode git-gutter magit counsel evil use-package))))
+   '(telephone-line winum flycheck-clj-kondo projectile vterm groovy-mode nord-theme highlight-numbers rainbow-mode diminish exec-path-from-shell csv-mode git-gutter magit counsel evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
